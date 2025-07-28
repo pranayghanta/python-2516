1c58d78ecd771bf4fe08e9e78213382aad9e3d9c
Pranay Ghanta <pranayghanta3333@gmail.com>
2025-07-28 18:40:14 +0530

    variables and datatypes

---
+++ 01_vars_data_types/myntra.py
@@ -0,0 +1,6 @@
+brand_name = 'RARE RABBIT'
+product_desc = "Men Rayos Slim Fit Tartan Check Cotton Shirt"
+product_rating = 4.6
+product_price = 1719
+product_sizes = [36,38,42]
+print(type(product_sizes))
