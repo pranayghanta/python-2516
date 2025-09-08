615acc5373b0036fcbd28cdfeef18b31482eac45
pranay <pranayghanta3333@gmail.com>
2025-09-08 16:54:46 +0530

    inheritance

---
+++ 15_inheritance/new.py
@@ -0,0 +1,86 @@
+# with inheritance
+class Student:
+    # video functionality watching
+    def watch_videos(self):
+        print("="*50)
+        print("Functionality for watching videos")
+        print("w")
+        print("a")
+        print("t")
+        print("c")
+        print("h")
+        print("i")
+        print("n")
+        print("g")
+        print("v")
+        print(".")
+        print(".")
+        print(".")
+        print(".")
+        print(".")
+        print(".")
+        print(".")
+        print("="*50)
+
+class VideoAdmin(Student):
+    # video functionality adding
+    def add_videos(self):
+            print("="*50)
+            print("Functionality for adding videos")
+            print("a")
+            print("d")
+            print("d")
+            print("i")
+            print("n")
+            print("g")
+            print("v")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print("="*50)
+
+class SuperAdmin(VideoAdmin):
+         # video functionality deleting
+    def delete_videos(self): 
+            print("="*50)
+            print("Functionality for deleting videos")
+            print("d")
+            print("e")
+            print("l")
+            print("e")
+            print("t")
+            print("e")
+            print("v")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print("="*50)
+
+# test functionalities
+print("Student user")
+student_user = Student()
+student_user.watch_videos()
+
+print("VideoAdmin User")
+va_user = VideoAdmin()
+va_user.watch_videos() # inherited from Student
+va_user.add_videos()
+
+print("Super Admin User")
+sa_user = SuperAdmin()
+sa_user.watch_videos() # inherited from Student
+sa_user.add_videos()   # inherited from VideoAdmin
+sa_user.delete_videos()
+
+
+
+
+
